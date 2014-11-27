7108a3a7a8fcf84a1f469c3ba6251cd6ad1bb358
hogueyy <chrishogue@utexas.edu>
2014-11-27 12:43:07 -0600

    more cost

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,12 @@ class User < ActiveRecord::Base
   validates :phone, presence: true,
                     length: { within: 10..14 }
                    # numericality: { only_integer: true }
+
+  # Returns the hash digest of the given string.
   def User.digest(string)
-    cost=ActiveModel::SecurePassword.min_cost? BCrypt::Engine::MIN_COST :
-                                               BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost:cost)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
