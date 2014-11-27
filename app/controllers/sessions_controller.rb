57670d044e0eb6e22c1c721b658dd1b3f1776faf
hogueyy <chrishogue@utexas.edu>
2014-11-26 23:03:43 -0600

    Session start

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid username/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
