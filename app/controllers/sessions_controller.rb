4b614baaf53abe60fb80fe3fb056c3683231f1ce
hogueyy <chrishogue@utexas.edu>
2014-11-29 22:44:16 -0600

    Added secutiry to edit users and friendly forwding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
-      # Log the user in and redirect to the user's show page.
+      redirect_back_or user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid username/password combination'
