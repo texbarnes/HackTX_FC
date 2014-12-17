cf3bd02bfec5fc22bf1a80f53e32478d4a62a31d
texbarnes <texbarnes@gmail.com>
2014-12-17 22:40:26 +0000

    MUSH MUSH MUSHY MUSH

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,18 +28,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  def vcard
+  def download
     @user = User.find(params[:id])
     send_data("BEGIN:VCARD
 VERSION:2.1
-N:<%= @user.last %>;<%= @user.first %> (Name)
-FN:<%= @user.first %><%= @user.last %> (Full Name)
-ORG:<%= @user.org %>
-TITLE:<%= @user.role %>
-TEL;WORK;VOICE:<%= @user.phone %>
-TEL;HOME;VOICE:<%= @user.phone %>
-EMAIL;PREF;INTERNET:<%= @user.email %>
-END:VCARD", :filename => "<%= @user.first %>.<%= @user.last %>.vcf")
+N:"<%= @user.last %>";"<%= @user.first %>"(Name)
+FN:"<%= @user.first %><%= @user.last %>"(Full Name)
+ORG:"<%= @user.org %>"
+TITLE:"<%= @user.role %>"
+TEL;WORK;VOICE:"<%= @user.phone %>"
+TEL;HOME;VOICE:"<%= @user.phone %>"
+EMAIL;PREF;INTERNET:"<%= @user.email %>"
+END:VCARD", :filename => <%= @user.first %>"."<%= @user.last %>".vcf")
   end  
   
   def index
