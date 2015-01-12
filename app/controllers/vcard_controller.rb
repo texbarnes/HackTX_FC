539574446847f3321b5439e46559895627d25a59
texbarnes <texbarnes@gmail.com>
2015-01-12 19:23:20 +0000

    changed vcard note 2

--- app/controllers/vcard_controller.rb
+++ app/controllers/vcard_controller.rb
@@ -7,27 +7,27 @@ class VcardController < ApplicationController
   
   def show
   
-  @contact = User.find(params[:id])  
+	  @contact = User.find(params[:id])  
+		
+		card = Vpim::Vcard::Maker.make2 do |maker|
+	  			
+			maker.add_name do |name|
+	    			name.prefix = ''
+	    			name.given = @contact.first
+	    			name.family = @contact.last
+			end
 	
-	card = Vpim::Vcard::Maker.make2 do |maker|
-  			
-		maker.add_name do |name|
-    			name.prefix = ''
-    			name.given = @contact.first
-    			name.family = @contact.last
+			maker.add_tel(@contact.phone)
+	
+			maker.add_email(@contact.email) 
+			
+			maker.add_url("www.forevercard.co/" + @contact.username)
+			
+			maker.add_note("If this contact is out of date, click the ForeverCard URL above to update. \n\n \n\n Thanks for using ForeverCard!")
+	
 		end
-
-		maker.add_tel(@contact.phone)
-
-		maker.add_email(@contact.email) 
-		
-		maker.add_url("www.forevercard.co/" + @contact.username)
-		
-		maker.add_note("If this contact is out of date, click the ForeverCard URL above to update. \r\n \r\n Thanks for using ForeverCard!")
-
-	end
-
-	send_data card.to_s, :filename => "contact.vcf"	
+	
+		send_data card.to_s, :filename => "contact.vcf"	
 	
   end
 end
