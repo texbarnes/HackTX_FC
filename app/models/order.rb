09b06cc992f80f95af6c28358766b028060dd4b8
texbarnes <texbarnes@gmail.com>
2015-01-18 19:28:06 +0000

    order multi form payment 80

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,11 @@
 class Order < ActiveRecord::Base
     has_attached_file :image
-    attr_accessible :paperStock, :quantity, :design, :image
+    attr_accessible :paperStock, :quantity, :design, :image, :address1, :address2, :city, :state, :zip, :shipping
+    
+    validates :address1, presence: true
+    validates :address2, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip, presence: true
     
 end
