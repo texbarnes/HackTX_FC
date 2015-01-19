9ffcfd6b33a187360ab6981b0fff67c96da11768
texbarnes <texbarnes@gmail.com>
2015-01-19 17:26:50 +0000

    pricing fix 14

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
-    belongs_to :user
     
     has_attached_file :image
     attr_accessible :paperStock, :quantity, :design, :image, :address1, :address2, :city, :state, :zip, :shipping, :order_date, :user_id
+     belongs_to :user
     
     validates :address1, presence: true
     validates :address2, presence: true
