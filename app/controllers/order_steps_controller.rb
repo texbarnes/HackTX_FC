c425af1d183e9a8aeb0147d2c30cb819c3b14b0f
texbarnes <texbarnes@gmail.com>
2015-01-19 17:53:54 +0000

    pricing fix 16

--- app/controllers/order_steps_controller.rb
+++ app/controllers/order_steps_controller.rb
@@ -15,7 +15,7 @@ class OrderStepsController < ApplicationController
     end
     
     def create
-        @order = Order.create(user_id: current_user.id)
+        @order = Order.new
         redirect_to wizard_path(steps.first, :order_id => @order.id)
     end
 end
