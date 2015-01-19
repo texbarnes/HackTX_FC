2d7230c9cdde29ef49fd7fbf8a16a81d4d3c6c34
texbarnes <texbarnes@gmail.com>
2015-01-19 20:52:57 +0000

    new order model 3

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ActiveRecord::Base
+    attr_accessible :quantity, :paperStock, :design, :subtotal, :total, :order_date, :address1, :address2, :state, :city, :zip, :user_id
+    has_attached_file :image
 end
