35181d9cc9be21c236865428456fd52354e61010
texbarnes <texbarnes@gmail.com>
2015-01-20 19:58:00 +0000

    multi form for user 16

--- app/helpers/user_steps_helper.rb
+++ app/helpers/user_steps_helper.rb
@@ -1,6 +1,6 @@
 module UserStepsHelper
     
     def generateTwitForm
-        @twitForm = "<div class='row'> <div class='form-group col-md-6'> <%= f.label 'Twitter' %> <%= f.check_box :showTwit %> </div> </div>"
+        @twitForm = "<div class='row'> <div class='form-group col-md-6'> <%= f.label 'Twitter' %> <%= f.check_box :showTwit %> </div> </div>".html_safe
     end    
 end
