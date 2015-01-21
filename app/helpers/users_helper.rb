65baa7da8884793b946fd0f1b1b201b3044b82f0
texbarnes <texbarnes@gmail.com>
2015-01-21 21:15:55 +0000

    multi step form user edit 19

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,10 @@ module UsersHelper
         number = "+1-#{sets_of_numbers.join('-')}"
         link_to image_tag("http://i57.tinypic.com/kf00tk.png"), "tel:#{number}"
     end
+    
+    def text_link(text)
+        sets_of_numbers = text.scan(/[0-9]+/)
+        number = "+1-#{sets_of_numbers.join('-')}"
+        link_to image_tag("http://i58.tinypic.com/2njbuw7.png"), "sms:#{number}"
+    end
 end
