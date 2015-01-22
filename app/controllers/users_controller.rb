1fddc8607c4a636159c1ba70f53d23c03a756190
texbarnes <texbarnes@gmail.com>
2015-01-22 19:54:18 +0000

    multi step form user edit 28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:first, :last, :email, :username, :password, :org, :role, :phone, :bio, :facebook, :twitter, :linkedin)
+    params.require(:user).permit(:last, :email, :username, :password, :org, :role, :phone, :bio, :facebook, :twitter, :linkedin)
   end
   
   def choose_layout
