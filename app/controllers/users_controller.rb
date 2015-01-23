589858a9bdb7a0ada0cc39b9957c76bf5e9c9714
texbarnes <texbarnes@gmail.com>
2015-01-23 04:21:39 +0000

    multi step form user edit 50

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     if(@user.classicMode)
       render 'classic'
     else
